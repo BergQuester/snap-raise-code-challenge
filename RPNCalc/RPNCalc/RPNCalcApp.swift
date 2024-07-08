7a04430f12948cfd136b8d0188732042ce1e352d
Daniel Bergquist <daniel.bergquist@hey.com>
2024-07-08 10:40:54 -0600

    Initial project commit

---
+++ RPNCalc/RPNCalc/RPNCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  RPNCalcApp.swift
+//  RPNCalc
+//
+//  Created by Daniel Bergquist on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct RPNCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
